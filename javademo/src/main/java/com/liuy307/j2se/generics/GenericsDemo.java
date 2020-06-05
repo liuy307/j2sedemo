eec929648c8950c3cb5816fc7a4475c5fd863e69
liuy307 <1774021266@qq.com>
2020-06-05 11:06:33 +0800

    新建javademo项目

---
+++ javademo/src/main/java/com/liuy307/j2se/generics/GenericsDemo.java
@@ -0,0 +1,24 @@
+package com.liuy307.j2se.generics;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class GenericsDemo {
+    @Test
+    //使用ArrayList时，如果不定义泛型类型时，泛型类型实际上就是Object：
+    public void test1() {
+        List list = new ArrayList();
+        list.add("Hello");
+        list.add("World");
+        int i = 1;
+        list.add(i);//自动装箱,因为list是object
+        list.add(new Date());
+        String first = (String) list.get(0);
+        String second = (String) list.get(1);
+        int third = (Integer) list.get(2);//自动拆箱
+        Date forth = (Date) list.get(3);
+    }
+}
