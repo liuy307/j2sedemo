c2df2a9a70c94f95fb725af90a6dc8b6baa02f5d
liuy307 <1774021266@qq.com>
2020-06-07 23:00:11 +0800

    进行泛型测试

--- javademo/src/main/java/com/liuy307/j2se/generics/GenericsDemo.java
+++ javademo/src/main/java/com/liuy307/j2se/generics/GenericsDemo.java
@@ -16,9 +16,14 @@ public class GenericsDemo {
         int i = 1;
         list.add(i);//自动装箱,因为list是object
         list.add(new Date());
+        if (list == null) {
+            ;
+        }
         String first = (String) list.get(0);
         String second = (String) list.get(1);
         int third = (Integer) list.get(2);//自动拆箱
         Date forth = (Date) list.get(3);
+        System.out.println("second = " + second);
+        System.out.println(forth);
     }
 }
