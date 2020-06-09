d834d9d2f227888aa38d1a0ee098978de4c23594
liuy307 <1774021266@qq.com>
2020-06-10 00:39:20 +0800

    增加ArrayListDemo，写出自己的简易版MyArrayList

---
+++ javademo/src/main/java/com/liuy307/j2se/collection/ArrayListDemo.java
@@ -0,0 +1,29 @@
+package com.liuy307.j2se.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+    @Test
+    public void test1() {
+        List<String> list1 = new ArrayList<>(0);
+        list1.add("21");
+        List<String> list2 = new ArrayList<>();
+        list2.add("2");
+    }
+
+    @Test
+    public void MyArrayListTest() {
+        MyArrayList list1 = new MyArrayList(0);
+        for (int i = 0; i < 10; i++) {
+            list1.add("21");
+        }
+
+        MyArrayList list2 = new MyArrayList();
+        for (int i = 0; i < 10; i++) {
+            list2.add("22");
+        }
+    }
+}
---
+++ javademo/src/main/java/com/liuy307/j2se/collection/MyArrayList.java
@@ -0,0 +1,49 @@
+package com.liuy307.j2se.collection;
+
+import java.util.Arrays;
+
+public class MyArrayList {
+    private static final Object[] EMPTY = {};
+    private static final Object[] DEFAULT_CAPCITY_EMPTY = {};
+    private final int DEFAULT_CAPCITY = 20;
+
+    private Object[] elementData;
+    int size;
+
+    public MyArrayList() {
+        this.elementData = DEFAULT_CAPCITY_EMPTY;
+    }
+
+    public MyArrayList(int initCapcity) {
+        if(initCapcity == 0)
+            this.elementData = EMPTY;
+        else
+            this.elementData = new Object[initCapcity];
+    }
+
+    boolean add(Object e) {
+        ensureCapcityInternal(size + 1);
+        elementData[size++] = e;
+        return true;
+    }
+
+    void ensureCapcityInternal(int minCapcity){
+        minCapcity = reviseCapcity(minCapcity);
+        if(minCapcity > elementData.length)//重要
+            grow(minCapcity);
+    }
+
+    int reviseCapcity(int minCapcity) {
+        if (this.elementData == DEFAULT_CAPCITY_EMPTY)
+            return DEFAULT_CAPCITY;
+        return minCapcity;
+    }
+
+    void grow(int minCapcity) {
+        int oldCapcity = elementData.length;
+        int newCapcity = oldCapcity + (oldCapcity>>1);
+        if (newCapcity < minCapcity)
+            newCapcity = minCapcity;
+        this.elementData = Arrays.copyOf(elementData, newCapcity);
+    }
+}
